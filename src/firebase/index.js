a2678480d38dd973bfdf65cd051df89316a9e8d4
Lagerloef Torrealba <lagerloef.torrealba@gmail.com>
2020-10-07 17:45:32 -0300

    se incorpora clase 13

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,6 @@
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 
-/*
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyB-gfzqhtfdhDnz_2aBY3aEM0P00UaXkuY",
-  authDomain: "computienda-coder.firebaseapp.com",
-  databaseURL: "https://computienda-coder.firebaseio.com",
-  projectId: "computienda-coder",
-  storageBucket: "computienda-coder.appspot.com",
-  messagingSenderId: "93341681611",
-  appId: "1:93341681611:web:5b5320f421c8cd5b50984e"
-});
-*/
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
